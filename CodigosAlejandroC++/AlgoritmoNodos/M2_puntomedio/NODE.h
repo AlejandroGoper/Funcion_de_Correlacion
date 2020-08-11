29fb80b7d4da172a9bc249a97b0c3d4f19615563
AlejandroGoper <ia.gomezperez@ugto.mx>
2020-08-10 23:02:41 -0500

    Metodos funcionales, punto medio y esquinas

---
+++ CodigosAlejandroC++/AlgoritmoNodos/M2_puntomedio/NODE.h
@@ -0,0 +1,269 @@
+#include "distancias.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+class NODE{
+    //Atributos
+    private:
+    Nodo **NODOSD;
+    Nodo **NODOSR;
+    Puntos *DATA;
+    Puntos *RAND;
+    float dimension_caja;
+    float d_max;
+    int num_particiones;
+    int num_bins;
+    int num_puntos;
+    //Métodos
+    private:
+    void localizar_nodos(Nodo **, Puntos *);
+    void agregar(Puntos *&, int &, float,float);
+    void calcular_distancias(Puntos *, int, Puntos*,int, float*, int);
+    void distancias_entre_nodos_puros(Nodo**,float*);
+    public:
+    NODE(Puntos*, Puntos*,Nodo **,Nodo**,float,float,int,int,int);
+    void mostrar_nodo();
+    void calcular_histogramas_puros(float *, float*);
+    void calcular_histogramas_mixtos(float*);
+    ~NODE();
+
+};
+//Constructor
+NODE::NODE(Puntos *_DATA, Puntos *_RAND, Nodo **_NODOD, Nodo **_NODOR,float _dimension_caja, float _d_max, int _num_particiones, int _num_bins, int _num_puntos){
+    DATA = _DATA;
+    RAND = _RAND;
+    NODOSD = _NODOD;
+    NODOSR = _NODOR;
+    dimension_caja = _dimension_caja;
+    d_max = _d_max;
+    num_particiones = _num_particiones;
+    num_bins = _num_bins;
+    num_puntos = _num_puntos;
+    localizar_nodos(NODOSD, DATA);
+    localizar_nodos(NODOSR, RAND);
+}
+//calcula la distancia entre dos arrays de puntos
+//util para calcular la distancia entre nodos diferentes
+void NODE::calcular_distancias(Puntos *array1, int n1, Puntos *array2, int n2, float *XX, int incremento){
+    int i, j, pos;
+    float d,x1,x2,y1,y2;
+    float ds = ((float)(num_bins))/d_max;
+    for (i = 0; i < n1; i++)
+    {
+        x1 = array1[i].x;
+        y1 = array1[i].y;
+        for (j = 0; j < n2; j++)
+        {
+            x2 = array2[j].x;
+            y2 = array2[j].y;
+            d = euclidean_dist2D(x1-x2,y1-y2);
+            if (d<d_max)
+            {
+                pos = (int)(d*ds);
+                XX[pos] += incremento;
+            }
+        }
+    }
+}
+
+// se cacula por fuerza bruta las distancias entre nodos
+void NODE::distancias_entre_nodos_puros(Nodo** nodo,float*HH){
+    float x_p, y_p, x, y, d_entre_nodos, d_anterior;
+    float dl = dimension_caja/((float)(num_particiones));
+    float sqrt2 = sqrt(2.0);
+    int fila_piv, columna_piv, aux, fila, columna;
+    for (fila_piv = 0; fila_piv < num_particiones; fila_piv++)
+    {
+        for (columna_piv = 0; columna_piv < num_particiones; columna_piv++)
+        {
+            //coordenadas del nodo pivote
+            x_p = nodo[fila_piv][columna_piv].coordenada.x;
+            y_p = nodo[fila_piv][columna_piv].coordenada.y;
+            //variable auxiliar para que el programa calcule distancias entre nodos separados
+            //Si el nodo pivote es N[0][2], los demas nodos comenzaran a contar a partir de N[0][3],...  
+            aux = columna_piv+1;
+            for (fila = fila_piv; fila < num_particiones; fila++)
+            {
+                for (columna = aux; columna < num_particiones; columna++)
+                {   
+                    //Coordenadas del segundo nodo
+                    x = nodo[fila][columna].coordenada.x;
+                    y = nodo[fila][columna].coordenada.y;
+                    d_entre_nodos = euclidean_dist2D(x_p-x,y_p-y);
+
+                    //si los nodos estan en la misma fila o columna el parámetro de corrección es d_entre_nodos - tamaño del nodo
+                    if ((fila_piv == fila) || (columna_piv == columna))
+                    {
+                        d_entre_nodos -= dl;
+                    }                   
+                    else
+                    {
+                        d_entre_nodos -= sqrt2*dl;
+                    }
+                    
+                    if (d_entre_nodos < d_max)
+                    {
+                        //calcular distancia entre los puntos que contienen esos nodos
+                        calcular_distancias(nodo[fila_piv][columna_piv].elementos, nodo[fila_piv][columna_piv].num_elementos, nodo[fila][columna].elementos, nodo[fila][columna].num_elementos,HH,2);
+                    }
+                }
+                aux = 0;
+            }
+
+        }
+    }
+}
+
+void NODE::calcular_histogramas_puros(float *DD, float *RR){
+    //calcular por fuerza bruta las distancias entre los elementos de cada nodo
+    int fila, columna, i,j,pos;
+    float dd,rr, ds = ((float)(num_bins))/d_max;
+    for (fila = 0; fila < num_particiones; fila++)
+    {
+        for (columna = 0; columna < num_particiones; columna++)
+        {
+            for (i = 0; i < NODOSD[fila][columna].num_elementos - 1; i++)
+            {
+                for (j = i+1; j < NODOSD[fila][columna].num_elementos; j++)
+                {
+                    //para DD
+                    dd = euclidean_dist2D(NODOSD[fila][columna].elementos[i].x-NODOSD[fila][columna].elementos[j].x,NODOSD[fila][columna].elementos[i].y-NODOSD[fila][columna].elementos[j].y);
+                    if (dd < d_max)
+                    {
+                        pos = (int)(dd*ds);
+                        DD[pos] += 2;
+                    }
+                    rr = euclidean_dist2D(NODOSR[fila][columna].elementos[i].x - NODOSR[fila][columna].elementos[j].x, NODOSR[fila][columna].elementos[i].y - NODOSR[fila][columna].elementos[j].y);       
+                    if (rr < d_max)
+                    {
+                        pos = (int)(rr*ds);
+                        RR[pos] += 2;
+                    }   
+                }
+            }        
+        }   
+    }
+ 
+    distancias_entre_nodos_puros(NODOSD,DD);
+    distancias_entre_nodos_puros(NODOSR,RR);
+    
+}
+
+void NODE::calcular_histogramas_mixtos(float* DR){
+    float x_p, y_p, x, y, d_entre_nodos, d_anterior;
+    int fila_piv, columna_piv, fila, columna;
+    for (fila_piv = 0; fila_piv < num_particiones; fila_piv++)
+    {
+        for (columna_piv = 0; columna_piv < num_particiones; columna_piv++)
+        {
+            //coordenadas del nodo pivote
+            x_p = NODOSD[fila_piv][columna_piv].coordenada.x;
+            y_p = NODOSD[fila_piv][columna_piv].coordenada.y;
+            for (fila = 0; fila < num_particiones; fila++)
+            {
+                for (columna = 0; columna < num_particiones; columna++)
+                {   
+                    //Coordenadas del segundo nodo
+                    x = NODOSR[fila][columna].coordenada.x;
+                    y = NODOSR[fila][columna].coordenada.y;
+                    d_entre_nodos = euclidean_dist2D(x_p-x,y_p-y);
+                    if (d_entre_nodos < d_max)
+                    {
+                        d_anterior = d_entre_nodos;
+                        //calcular distancia entre puntos de los diferentes nodos
+                        calcular_distancias(NODOSD[fila_piv][columna_piv].elementos, NODOSD[fila_piv][columna_piv].num_elementos, NODOSR[fila][columna].elementos, NODOSR[fila][columna].num_elementos,DR,1);
+                    }
+                    else
+                    {   
+                        if (d_anterior < d_max)
+                        {
+                            //calcular distancia entre esos nodos
+                            calcular_distancias(NODOSD[fila_piv][columna_piv].elementos, NODOSD[fila_piv][columna_piv].num_elementos, NODOSR[fila][columna].elementos, NODOSR[fila][columna].num_elementos,DR,1);        
+                        }
+                        d_anterior = d_entre_nodos;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+//encuentra las posiciones de los nodos relativas a la dimension de la caja, inicializa arrays de elementos y el numero de elementos actuales es 0.
+//ademas asigna los datos a cada nodo
+void NODE::localizar_nodos(Nodo **nodos, Puntos *datos){
+    int i, fila, columna;
+    float dl = dimension_caja/((float)(num_particiones)), ds = 1.0/dl;
+    // creacion de nodos e inicializacion de valores y arrays de elementos
+    for (fila = 0; fila < num_particiones; fila++)
+    {
+        for (columna = 0; columna < num_particiones; columna++)
+        {
+            nodos[fila][columna].coordenada.y = ((float)(fila) + 0.5)*dl;
+            nodos[fila][columna].coordenada.x = ((float)(columna) + 0.5)*dl; 
+            nodos[fila][columna].num_elementos = 0; // todos los nodos tiene 0 puntos
+            nodos[fila][columna].elementos = new Puntos[0]; // todos los elementos son arreglos de dimension 0
+        }
+    }
+    //asignacion dinamica de puntos a cada nodo.
+    for (i = 0; i < num_puntos; i++)
+    {
+        columna = (int)(datos[i].x*ds);
+        fila = (int)(datos[i].y*ds);
+        agregar(nodos[fila][columna].elementos,nodos[fila][columna].num_elementos,datos[i].x, datos[i].y);
+    }
+
+}
+//Destructor
+NODE::~NODE(){
+}
+
+void NODE::mostrar_nodo(){
+    int i,j,c;
+    for(i = 0; i < num_particiones; i++)
+    {
+        for ( j = 0; j < num_particiones; j++)
+        {
+            printf("NodoD [%d,%d] = (%.2f,%.2f)\nTiene %d elementos: \n",i,j,NODOSD[i][j].coordenada.x, NODOSD[i][j].coordenada.y, NODOSD[i][j].num_elementos);
+            for (c = 0; c < NODOSD[i][j].num_elementos; c++)
+            {
+                printf("punto: (%f, %f) \n",NODOSD[i][j].elementos[c].x, NODOSD[i][j].elementos[c].y);
+            }
+            
+        }
+        
+    }
+    printf("\n\n\n\n");
+    for(i = 0; i < num_particiones; i++)
+    {
+        for ( j = 0; j < num_particiones; j++)
+        {
+            printf("NodoR [%d,%d] = (%.2f,%2f)\nTiene %d elementos: \n",i,j,NODOSR[i][j].coordenada.x, NODOSR[i][j].coordenada.y, NODOSR[i][j].num_elementos);
+            for (c = 0; c < NODOSR[i][j].num_elementos; c++)
+            {
+                printf("punto: (%f, %f) \n",NODOSR[i][j].elementos[c].x, NODOSR[i][j].elementos[c].y);
+            }
+            
+        }
+        
+    }
+    
+
+}
+
+// agrega un elemento al final del array de tipo puntos e incrementa su longitud en 1
+void NODE::agregar(Puntos *&array, int &longitud, float _x, float _y){
+    longitud++;
+    Puntos *array_auxiliar = new Puntos[longitud];
+    int i;
+    for (i = 0; i < longitud-1; i++)
+    {
+        array_auxiliar[i].x = array[i].x;
+        array_auxiliar[i].y = array[i].y;
+    }
+    delete[] array;
+    array = array_auxiliar;
+    array[longitud-1].x = _x;
+    array[longitud-1].y = _y;
+}
---
+++ CodigosAlejandroC++/AlgoritmoNodos/M2_puntomedio/main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <ctime>
+#include "NODE.h"
+
+using namespace std;
+
+Puntos *DATA,*RAND;
+Nodo **NODOSD,**NODOSR;
+float *DD,*RR,*DR;
+
+void abrir_archivo(string,int, Puntos *);
+void construir_nodos(int);
+void construir_histogramas(int);
+void guardar_Histograma(string,int, float*);
+void eliminar_Datos();
+
+/* 
+
+ARGUMENTOS DEL MAIN:
+1 - Nombre del archivo de DATOS
+2 - Nombre del archivo de RANDOM
+3 - Numero de puntos en los archivos anteriores (int)
+4 - Numero para los nodos (int), si es 4 por ejemplo, habra 4x4 nodos
+5 - Tamaño de la caja (float)
+6 - Numero de bins para los histogramas (int)
+7 - Distancia máxima (float) 
+
+*/
+
+int main(int argc, char **argv){
+    time_t to, tf;
+    //construccion de nombres de archivos a exportar
+    string nombre1 = "DDiso_2D";
+    string nombre2 = "DRiso_2D";
+    string nombre3 = "RRiso_2D";
+    nombre1.append(argv[3]);
+    nombre2.append(argv[3]);
+    nombre3.append(argv[3]);
+    nombre1 += ".dat";
+    nombre2 += ".dat";
+    nombre3 += ".dat";
+    //asignando argumentos del main a variables 
+    int N_puntos = stoi(argv[3]);
+    int N_particiones = stoi(argv[4]);
+    float dim_caja = stof(argv[5]);
+    int N_bins = stoi(argv[6]);
+    float d_max = stof(argv[7]);
+    //CREANDO ARREGLOS DE DATOS
+    DATA = new Puntos[N_puntos];
+    RAND = new Puntos[N_puntos];
+    //Cargando los datos a sus respectivos STRUCT
+    abrir_archivo(argv[1],N_puntos,DATA);
+    abrir_archivo(argv[2],N_puntos,RAND);
+    //Creando Histogramas
+    construir_histogramas(N_bins); 
+    //Creando nodos
+    construir_nodos(N_particiones);
+    //Instacia de la clase
+    NODE obj(DATA,RAND,NODOSD,NODOSR,dim_caja,d_max,N_particiones,N_bins,N_puntos);
+    to = time(NULL);
+    //calculo los histogramas
+    obj.calcular_histogramas_puros(DD,RR);
+    obj.calcular_histogramas_mixtos(DR);
+    tf = time(NULL);
+    obj.~NODE(); // destruyo objeto
+    guardar_Histograma(nombre1,N_bins,DD);
+    guardar_Histograma(nombre2,N_bins,DR);
+    guardar_Histograma(nombre3,N_bins,RR);
+    eliminar_Datos(); // destruyo structs e histogramas
+    printf("Terminado en: %.4f segundos.",difftime(tf,to));
+    return 0;
+}
+
+void construir_histogramas(int dim){
+    DD = new float[dim];
+    RR = new float[dim];
+    DR = new float[dim];
+    int i;
+    for (i = 0; i < dim; i++)
+    {
+        *(DD+i) = 0.0;
+        *(DR+i) = 0.0;
+        *(RR+i) = 0.0;
+    }
+}
+
+void construir_nodos(int n){
+    NODOSD = new Nodo*[n];
+    NODOSR = new Nodo*[n];
+    for (int i = 0; i < n; i++)
+    {
+        *(NODOSD + i) = new Nodo[n];
+        *(NODOSR + i) = new Nodo[n];
+    }
+}
+
+
+//toma los datos del archivo y los guarda en un arreglo de structuras.
+void abrir_archivo(string nombre_archivo,int cantidad_puntos, Puntos *datos){
+    ifstream archivo;
+    archivo.open(nombre_archivo.c_str(), ios::in | ios::binary); //le indico al programa que se trata de un archivo binario con ios::binary
+    if (archivo.fail()){
+        cout << "Error al cargar el archivo " << endl;
+        exit(1);
+    }
+    int c=0;
+    float eliminar1, eliminar2;
+    while (!archivo.eof())
+    {
+        archivo >> datos[c].x >> datos[c].y >> eliminar1 >> eliminar2; 
+        c++;
+    }
+    archivo.close();
+}
+
+void guardar_Histograma(string nombre,int dim, float*histograma){
+    ofstream archivo;
+    archivo.open(nombre.c_str(),ios::out | ios::binary);
+    if (archivo.fail()){
+        cout << "Error al guardar el archivo " << endl;
+        exit(1);
+    }
+    for (int i = 0; i < dim; i++)
+    {
+        archivo << histograma[i] << endl;
+    }
+    archivo.close();
+}
+
+
+void eliminar_Datos(){
+    delete[] DATA;
+    delete[] RAND;
+    delete[] DD;
+    delete[] RR;
+    delete[] DR;
+}
